746c7847c76887edffacf14c6d9c5be0aee0bff0
mcpadma <jmcpadma@gmail.com>
2020-05-07 22:59:07 +0530

    component communication, using multiple local states

--- handlingevents/src/App.js
+++ handlingevents/src/App.js
@@ -7,16 +7,31 @@ const initList = [
   { name: "tomato", calories: 20},
   { name: "rice", calories: 40},
   { name: "chocolate", calories: 100}
-]
+];
 
 function App() {
   const [list, setList] = useState(initList);
-  const removeUnhealthyHandle = (e) =>{
+  const [editable, setEditable] = useState(false);
+
+  const removeUnhealthyHandle = (e) => {
     //const copyList = {...list};
     const filteredList = list.filter( v => v.calories < 50);
     setList(filteredList);
   }
 
+  function removeItemHandle(e)  {
+    console.dir(e.target.name);
+    const filteredList = list.filter( v => v.name != e.target.name);
+    setList(filteredList);
+  }
+  function onDoubleClickHandle() {
+    setEditable(true);
+  }
+//  function removeUnhealthyHandle(e) {
+//     //const copyList = {...list};
+//     const filteredList = list.filter( v => v.calories < 50);
+//     setList(filteredList);
+//   }
   return (
     <div className="App">
       <header className="App-header">
@@ -24,10 +39,16 @@ function App() {
        {
          list.map((v,k) => {
           //  return <Item name = {v.name} calorie = {v.calories} key={k}></Item>
-          return <Item item = {v} key={`${k}${v.name}${v.calories}`}></Item>
+          return <Item 
+          item = {v} 
+          key={`${k}${v.name}${v.calories}`} 
+          onClick={removeItemHandle}
+          editable={editable}
+          onDoubleClick={onDoubleClickHandle}></Item>
          })
        }
-       <button className="remove-button" onClick = {removeUnhealthyHandle}> Remove Unhealthy</button>
+        {/* <button className="remove-button" onClick = {removeUnhealthyHandle}> Remove Unhealthy</button> */}
+      
       </header>
     </div>
   );
--- handlingevents/src/components/item.js
+++ handlingevents/src/components/item.js
@@ -6,8 +6,15 @@ return(
     <div className="itemName">
         {/* <h3>Name: {props.name}</h3>
         <h3>Calories: {props.calorie}</h3> */}
-        <h3>Name: {props.item.name}</h3>
+        {
+            props.editable ? 
+            <input type="text" defaultValue={props.item.name}/> 
+            : 
+            <h3 onDoubleClick = {props.onDoubleClick}>Name: {props.item.name}</h3>
+        }
+        
         <h3>Calories: {props.item.calories}</h3>
+        <button name={props.item.name} className="remove-button" onClick = {props.onClick}> Remove </button>
     </div>
  )
 }

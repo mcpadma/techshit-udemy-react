04df4e68e4820ebfaf2e225f0ef8dd1ccbdc37ba
mcpadma <jmcpadma@gmail.com>
2020-07-29 09:39:18 +0530

    router redirect and prompt on redirect with menu styles

--- react-routers/src/App.js
+++ react-routers/src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  NavLink,
+  Redirect,
+  Prompt
+} from "react-router-dom";
 import "./App.css";
 
 import AboutPage from "./pages/AboutPage";
 import UserPage from "./pages/UserPage";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [age, setAge] = useState('');
+
+  function loginHandle() {
+    setLoggedIn(!loggedIn);
+  }
+  function onChangeHandle(e) {
+    setAge(e.target.value);
+  }
   return (
     <BrowserRouter>
       <div className="App">
@@ -21,53 +37,84 @@ function App() {
           return<h1>Welcome About Page</h1>
         }}></Route> */}
           <Route path="/about" exact component={AboutPage}></Route>
-          <Route
+          {/* <Route
             path="/user/:username"
             exact
             render={({ match }) => {
               return <h1>Welcome {match.params.username}</h1>;
             }}
-          ></Route>
-          {/* <Route path="/user/:firstname/:lastname" exact 
-        render={({match})=>{
-          return <h1>Welcome {match.params.firstname} {match.params.lastname}</h1>
-        }}></Route> */}
+          ></Route> */}
+          <Prompt
+            when={loggedIn && !age}
+            message={location => {
+              return location.pathname.startsWith("/user")
+                ? true
+                : "Are you sure?";
+            }}
+          ></Prompt>
           <Route
             path="/user/:firstname/:lastname"
             exact
-            component={UserPage}
+            render={({ match }) => {
+              return loggedIn ? (
+                <div>
+                  <h2>age: {age}</h2>
+                  <input
+                    type="text"
+                    value={age}
+                    onChange={onChangeHandle}
+                  ></input>
+                  <h1>Welcome {match.params.firstname} {match.params.lastname} </h1>
+                </div>
+              ) : (
+                <Redirect to="/"></Redirect>
+              );
+            }}
           ></Route>
+          {/* <Route
+            path="/user/:firstname/:lastname"
+            exact
+            component={UserPage}
+          ></Route> */}
 
           {/* Router links */}
-          <ul>
-            <li>
+          <ul className="ul-style">
+            <li className="li-style">
               <NavLink
                 className="App-link"
-                to="/" exact
+                to="/"
+                exact
                 activeClassName="Link-active-style"
               >
                 Home
               </NavLink>
             </li>
-            <li>
+            <li className="li-style">
               <NavLink
                 className="App-link"
-                to="/about" exact
+                to="/about"
+                exact
                 activeClassName="Link-active-style"
               >
                 About
               </NavLink>
             </li>
-            <li>
+            <li className="li-style">
               <NavLink
                 className="App-link"
-                to="/user/padma/chockalingam" exact
+                to="/user/padma/chockalingam"
+                exact
                 activeStyle={{ color: "green" }}
               >
                 User Page
               </NavLink>
             </li>
           </ul>
+          
+          {/* {loggedIn.toString()} */}
+          <button className="button" onClick={loginHandle}>
+            {loggedIn ? "LogOut" : "Login"}
+          </button>
         </header>
       </div>
     </BrowserRouter>
